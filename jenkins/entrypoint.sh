22d3d6c9604c80dee99e977810f50eaa50309bf5
Marc Gaines <marc.gaines@gaines-solutions.de>
2017-07-16 18:07:55 +0200

    no message

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Lauching jenkins ..."
+echo "--------> Lauching jenkins ..."
 /bin/tini -- /usr/local/bin/jenkins.sh
 
-echo "Lauching docker daemon ..."
-dind dockerd
+echo "--------> Lauching docker daemon ..."
+dockerd
 
 while(! docker info > /dev/null 2>&1); do
-    echo "Waiting for the docker daemon ..."
+    echo "--------> Waiting for the docker daemon ..."
     sleep 1
 done
-echo "Docker daemon is up and running!"
+echo "--------> Docker daemon is up and running!"
